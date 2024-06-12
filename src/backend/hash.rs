c1062802789963829d29706ed9b4b76349c538eb
maxgmr <mxzmxz120@gmail.com>
2024-06-12 16:35:47 -0700

    rename repo

--- src/backend/hash.rs
+++ src/backend/hash.rs
@@ -1,6 +1,6 @@
 use sha2::Sha256;
 
-/// The possible hash functions that diegruft can use.
+/// The possible hash functions that dgruft can use.
 pub enum HashFn {
     /// SHA-256 from [sha2] crate.
     Sha256,
