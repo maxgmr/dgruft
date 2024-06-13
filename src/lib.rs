9ee5bbda45194e2913fa30398f66fca0bdda9033
maxgmr <mxzmxz120@gmail.com>
2024-06-13 10:05:25 -0700

    rename to Hashed

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
+//! Modules utilised by dgruft.
 #![warn(missing_docs)]
 
+/// Backend code for dgruft.
 pub mod backend;
 #[cfg(feature = "frontend")]
+/// Frontend code for dgruft.
 pub mod frontend;
