92c6f2143f18d76bcb908b59a922df243f20f873
maxgmr <mxzmxz120@gmail.com>
2024-06-13 22:39:49 -0700

    refactor + doc is_base64

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,12 +1,9 @@
 //! Small, general helper functions.
 use regex::Regex;
 
+/// Return `true` iff the input string is parseable as a standard base 64-encoded string.
 pub fn is_base64(string: &str) -> bool {
     let base64_re =
         Regex::new(r"^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$").unwrap();
-    if let Some(_) = base64_re.captures(string) {
-        true
-    } else {
-        false
-    }
+    base64_re.captures(string).is_some()
 }
