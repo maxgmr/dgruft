4ddfb17f297bedcd0aaae731e944d3db2318e1bd
maxgmr <mxzmxz120@gmail.com>
2024-06-24 15:01:49 -0700

    create crate hierarchy

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,12 @@
 //! Storage, editing, reading, and writing of encrypted files and credentials.
 #![warn(missing_docs)]
+
+/// Backend functionality.
+mod backend;
+/// CLI interface.
+mod cli;
+#[cfg(feature = "frontend")]
+/// GUI interface.
+mod frontend;
+/// General program utilities.
+mod utils;
