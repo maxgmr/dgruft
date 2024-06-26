43ef841a26a82c3ea8bf9b97fe5507708bb096fc
maxgmr <mxzmxz120@gmail.com>
2024-06-25 19:09:20 -0700

    init sql schemas

---
+++ src/backend/vault/sql_schemas.rs
@@ -0,0 +1,5 @@
+pub const CREATE_ACCOUNTS: &str = "CREATE TABLE IF NOT EXISTS accounts ()";
+
+pub const CREATE_FILES_DATA: &str = "CREATE TABLE IF NOT EXISTS files_data ()";
+
+pub const CREATE_CREDENTIALS: &str = "CREATE TABLE IF NOT EXISTS credentials ()";
