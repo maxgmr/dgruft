7279d597f320edd4bd4cfc7899bbb44200ee972d
maxgmr <mxzmxz120@gmail.com>
2024-06-25 21:52:50 -0700

    project structure

--- src/backend.rs
+++ src/backend.rs
@@ -1,5 +1,7 @@
 //! All backend functionality.
 mod account;
 mod credentials;
+mod encryption;
 mod file_data;
+mod hashing;
 mod vault;
---
+++ src/backend/vault.rs
@@ -0,0 +1,4 @@
+mod database;
+mod filesystem;
+mod sql_schemas;
+mod sql_statements;
