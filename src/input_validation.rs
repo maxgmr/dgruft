cda30b4a94719782d3f0b7d2541b2703614d4b5e
maxgmr <mxzmxz120@gmail.com>
2024-07-02 14:10:04 -0700

    create input validation

--- src/input_validation.rs
+++ src/input_validation.rs
@@ -1,1 +1,33 @@
 //! Different helpers, structs, and consts for user input validation.
+use color_eyre::eyre::{self, eyre};
+
+/// Ensure that the given [String] satisfies the given restrictions.
+pub fn validate_input(
+    input: String,
+    min_len: usize,
+    max_len: usize,
+    forbidden_chars: &str,
+) -> eyre::Result<()> {
+    if input.len() < min_len {
+        Err(eyre!(
+            "Input length {} is less than minimum length {}.",
+            input.len(),
+            min_len
+        ))
+    } else if input.len() > max_len {
+        Err(eyre!(
+            "Input length {} is greater than maximum length {}.",
+            input.len(),
+            max_len
+        ))
+    } else {
+        for f_char in forbidden_chars.chars() {
+            for i_char in input.chars() {
+                if i_char == f_char {
+                    return Err(eyre!("Input cannot contain character '{}'.", i_char));
+                }
+            }
+        }
+        Ok(())
+    }
+}
