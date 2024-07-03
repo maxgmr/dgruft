9363379b003fb616f68e3655c24b10073328d7f4
maxgmr <mxzmxz120@gmail.com>
2024-07-03 12:05:28 -0700

    create config template

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,12 @@
 //! All functionality related to user customisation of `dgruft`.
+use color_eyre::eyre;
 
 /// All the user-defined options.
 #[derive(Debug, Default)]
 pub struct Config {}
+impl Config {
+    pub fn new() -> eyre::Result<Self> {
+        // TODO
+        Ok(Config {})
+    }
+}
